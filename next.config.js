50982c6e575a7d9e5de29431754163c39b4d3098
Eric Kim <bosung90@gmail.com>
2023-12-22 15:13:22 -0800

    add sharp for image optimization. fix next build

--- next.config.js
+++ next.config.js
@@ -9,4 +9,10 @@ module.exports = {
       },
     ],
   },
+  // Error on build. TypeError: Expected signal to be an instanceof AbortSignal
+  // See: https://github.com/triggerdotdev/trigger.dev/issues/806
+  experimental: {
+    serverMinification: false,
+  },
+  // =======================================================
 }
