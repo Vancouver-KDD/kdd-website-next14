c0333fa799f683aef4a580caed9a15087801b0f3
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-12-23 01:08:16 -0800

    Fix build error

--- app/(page)/blogs/page.tsx
+++ app/(page)/blogs/page.tsx
@@ -20,7 +20,7 @@ const Route = () => {
     <div className="max-w-4xl md:max-w-6xl flex flex-col justify-center items-center gap-8 m-auto p-4 pt-48 md:pt-32">
       <Section title="Blogs" >
           {blogs.map((blog, index) => (
-            <Grid container className="mb-5">
+            <Grid container className="mb-5" key={index}>
                 <BlogCard blog={blog} index={index} />
             </Grid>
           ))}
