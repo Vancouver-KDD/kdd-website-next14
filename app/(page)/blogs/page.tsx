e3d6fbd2f99b71a91060d66845f7eb942e8df220
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-12-22 19:40:10 -0800

    feat: Fetch latest blog data & display on blog page

--- app/(page)/blogs/page.tsx
+++ app/(page)/blogs/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { getLatestBlogs } from '@/app/actions/airtable'
+import { Section } from '@/components'
 
-type Props = {}
+const Route = () => {
+  const [blogs, setBlogs] = useState<DB.Blog[]>([])
 
-const route = (props: Props) => {
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const blogs = await getLatestBlogs({limit: 20})
+      setBlogs(blogs)
+    }
+    fetchBlogs()
+  }, [])
+
+  console.log('브ㄹ로그', blogs)
+
   return (
-    <div>route</div>
+    <div className="max-w-4xl md:max-w-5xl flex flex-col justify-center items-center gap-8 m-auto p-4 pt-56">
+      <Section title="Blogs" className="text-center">
+        {blogs.map((blog) => (
+            <div className="">
+            <p className=" text-gray-800 text-xl font-bold">
+              {blog.title ?? ''}
+            </p>
+            <p className="line-clamp-4 text-gray-500 text-base my-2">
+              {blog.description ?? ''}
+            </p>
+            </div>
+        ))}
+      </Section>
+    </div>
   )
 }
 
-export default route
+export default Route
