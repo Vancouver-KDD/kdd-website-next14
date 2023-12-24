2b1245dc6fb58da8ec134b31f308eb3aaa46afd8
Eric Kim <bosung90@gmail.com>
2023-12-23 17:28:29 -0800

    Make blogs page nav instantly before loading blog posts

--- app/(pages)/blogs/page.tsx
+++ app/(pages)/blogs/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import {getLatestBlogs} from '@/actions/airtable'
 import {Section} from '@/components'
 import BlogCard from './BlogCard'
@@ -8,18 +8,24 @@ import type {Metadata} from 'next'
 export const metadata: Metadata = {
   title: 'Blogs 블로그',
 }
-export default async function BlogsPage() {
-  const blogs = await getLatestBlogs({limit: 20})
-
+export default function BlogsPage() {
   return (
     <div className="max-w-4xl md:max-w-6xl flex flex-col justify-center items-center gap-8 m-auto p-4 pt-48 md:pt-32">
       <Section title="Blogs">
-        {blogs.map((blog, index) => (
-          <Grid container className="mb-5" key={blog.id}>
-            <BlogCard blog={blog} index={index} />
-          </Grid>
-        ))}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Blogs />
+        </Suspense>
       </Section>
     </div>
   )
+}
+
+async function Blogs() {
+  const blogs = await getLatestBlogs({limit: 20})
+
+  return blogs.map((blog, index) => (
+    <Grid container className="mb-5" key={blog.id}>
+      <BlogCard blog={blog} index={index} />
+    </Grid>
+  ))
 }
